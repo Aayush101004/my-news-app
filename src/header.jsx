42c18562c0c4c6b1082b748d39be01a16d9a3340
Aayush101004 <aayush.khanna2602@gmail.com>
2024-03-28 20:23:50 +0530

    test1

--- src/header.jsx
+++ src/header.jsx
@@ -29,7 +29,6 @@ const Header = () => {
                     <li><a href="/about-us">About Us</a></li>
                 </ul>
             </nav>
-            <p>fsgjdsfg</p>
         </header>
 
     );
