64f1075024ae60a00685dc7437b42bd69175024c
Aayush101004 <aayush.khanna2602@gmail.com>
2024-04-22 18:56:27 +0530

    try5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import Navbar from './components/navbar/Navbar'
-import News from './components/news/News'
+import React from "react"
+import Header from "./components/common/header/Header"
+import "./App.css"
+import Homepages from "./components/home/Homepages"
+import Footer from "./components/common/footer/Footer"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import SinglePage from "./components/singlePage/SinglePage"
+import Culture from "./components/culture/Culture"
 
 const App = () => {
   return (
     <>
-      <Navbar/>
-      <News/>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Homepages} />
+          <Route path='/singlepage/:id' exact component={SinglePage} />
+          <Route exact path='/culture' component={Culture} />
+        </Switch>
+        <Footer />
+      </Router>
     </>
   )
 }
