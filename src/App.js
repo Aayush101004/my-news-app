3bf13030b2f04e108a7a534453ddfd87a4c1d1f9
Aayush101004 <aayush.khanna2602@gmail.com>
2024-04-22 18:25:12 +0530

    trying

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Bollywood from './bollywood.jsx';
-import Footer from './footer';
-import Header from './header';
-import Hollywood from './hollywood';
-import Main from './main';
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import News from './components/news/News'
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/bollywood.jsx" element={<Bollywood />} />
-          <Route path="/hollywood" element={<Hollywood />} />
-          {/* Other routes go here */}
-          <Route path="/" element={<Main />} />
-        </Routes>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+    <>
+      <Navbar/>
+      <News/>
+    </>
+  )
 }
 
-export default App;
+export default App
